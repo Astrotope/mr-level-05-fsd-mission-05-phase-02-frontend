777b8bd93a376869cd982c9e7e4331061e749d29
Cameron McEwing <cameronm@missionreadyhq.com>
2025-01-03 10:32:29 +1300

    Checkpoint 2025-01-02T21:32:28+0000 - add docker compose for production and development

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Changed to root path
+  base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve('./src'),
     },
   },
 })
