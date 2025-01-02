fd3b92d9332006902a4567886a7b93e27154f033
Cameron McEwing <cameronm@missionreadyhq.com>
2025-01-03 09:45:59 +1300

    Checkpoint 2025-01-02T20:45:59+0000 - initial state

--- save_rollback_point.sh
+++ save_rollback_point.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# Parse command line options
+COMMENT=""
+while getopts "m:" opt; do
+    case $opt in
+        m)
+            COMMENT="$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
 # Get current date in ISO format
 DATETIME=$(date -u +"%Y-%m-%dT%H:%M:%S%z")
 
 # Create git commit
 git add .
-git commit -m "Checkpoint ${DATETIME}"
+git commit -m "Checkpoint ${DATETIME}${COMMENT:+ - }${COMMENT}"
 
 # Get the commit hash
 COMMIT_HASH=$(git rev-parse HEAD)
 
 # Append to ROLLBACK_POINTS.md
-echo -e "\n## ${DATETIME}\nCommit Hash: ${COMMIT_HASH}" >> ROLLBACK_POINTS.md
+{
+    echo -e "\n## ${DATETIME}"
+    [ ! -z "$COMMENT" ] && echo "# ${COMMENT}"
+    echo "Commit Hash: ${COMMIT_HASH}"
+} >> ROLLBACK_POINTS.md
 
 echo "Created commit ${COMMIT_HASH} and recorded in ROLLBACK_POINTS.md"
